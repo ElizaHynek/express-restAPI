44c415734e5e45df5eceb691aeed5356dfec470b
Eliza Hynek <eliza.hynek@gmail.com>
2023-07-07 12:09:28 +0200

    group endpoints with express.router

--- server.js
+++ server.js
@@ -1,66 +1,23 @@
 const express = require('express');
-const cors = require('cors');
 const uuid = require('uuid');
+const router = express.Router();
 
 const app = express();
 
-app.use(cors());
+const testimonialsRoutes = require('./routes/testimonials.routes.js');
+const concertsRoutes = require('./routes/concerts.routes.js');
+const seatsRoutes = require('./routes/seats.routes.js');
+
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+app.use('/api/', testimonialsRoutes);
+app.use('/api/', concertsRoutes);
+app.use('/api/', seatsRoutes);
 
-const db = [
-  { id: 1, author: 'John Doe', text: 'This company is worth every coin!' },
-  { id: 2, author: 'Amanda Doe', text: 'They really know how to make you happy.' },
-];
-
-app.get('/testimonials/random', (req, res) => {
-  res.json(db[Math.floor(Math.random() * db.length)]);
-});
-
-app.get('/testimonials', (req, res) => {
-  res.json(db);
-});
-
-app.get('/testimonials/:id', (req, res) => {
-  res.json(db.find((data) => data.id == req.params.id));
-});
-
-app.post('/testimonials', (req, res) => {
-	//const { author, text } = req.body;
-	//const id = uuid();
-	//const newTestimonial = { id: id, author: author, text: text };
-	//db.push(newTestimonial);
-	res.json({ message: 'ok!' });
-});
-
-app.put('/testimonials/:id',(req, res) => {
-	const { author, text } = req.body;
-	const id = +req.params.id;
-	const testimontial = db.find((testimontial) => testimontial.id === id);
-	testimontial.author = author;
-	testimontial.text = text;
-	res.json({ message: 'ok!' })},
-	(err) => {
-		console.log(err);
-	}
-);
-
-app.delete('/testimonials/:id',(req, res) => {
-	const id = +req.params.id;
-	db.splice(
-			db.findIndex((testimontial) => testimontial.id === id),
-			1
-	);
-	res.json({ message: 'deleted' });
-	},
-	(err) => {
-		console.log(err);
-	}
-);
 
 app.use((req, res) => {
-  res.status(404).send('404 not found...');
+  res.status(400).send('Not found..');
 });
 
 app.listen(8000, () => {
