0dcb6b4cfe6358e71569cbf7120d1f3bf64f5443
Eliza Hynek <eliza.hynek@gmail.com>
2023-07-10 16:31:18 +0200

    changes for replit - server with errors

--- client/src/components/features/SeatChooser/SeatChooser.js
+++ client/src/components/features/SeatChooser/SeatChooser.js
@@ -11,10 +11,10 @@ const SeatChooser = ({ chosenDay, chosenSeat, updateSeat }) => {
   
   useEffect(() => {
     dispatch(loadSeatsRequest());
-    
-    const interval = setInterval(() => {
+    let interval;
+    interval = setInterval(() => {
       dispatch(loadSeatsRequest());
-    }, 120000);
+    }, 120000); 
     return () => clearInterval(interval)
   }, [dispatch])
 
--- server.js
+++ server.js
@@ -1,25 +1,32 @@
 const express = require('express');
+const cors = require('cors');
 const uuid = require('uuid');
 const router = express.Router();
+const path = require('path');
 
 const app = express();
 
-const testimonialsRoutes = require('./routes/testimonials.routes.js');
-const concertsRoutes = require('./routes/concerts.routes.js');
-const seatsRoutes = require('./routes/seats.routes.js');
+const testimonialsRoutes = require('./routes/testimonials.routes');
+const concertsRoutes = require('./routes/concerts.routes');
+const seatsRoutes = require('./routes/seats.routes');
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(cors());
+app.use(express.static(path.join(__dirname, '/client/build')));
 
 app.use('/api/', testimonialsRoutes);
 app.use('/api/', concertsRoutes);
 app.use('/api/', seatsRoutes);
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/client/build/index.html'));
+});
 
 app.use((req, res) => {
   res.status(400).send('Not found..');
 });
 
-app.listen(8000, () => {
+app.listen(process.env.PORT || 8000, () => {
   console.log('Server is running on port: 8000');
 });
