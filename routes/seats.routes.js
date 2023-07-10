59aeeee2850649bd78f0d2440b8d86df1c27110a
Eliza Hynek <eliza.hynek@gmail.com>
2023-07-10 16:41:33 +0200

    fix server (seats.routes)

--- routes/seats.routes.js
+++ routes/seats.routes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const db = require('../db');
 const router = express.Router();
-const uuid = require('uuid');
+const uuid = require('uuid').v4;
 
 router.route('/seats').get((req, res) => {
   res.json(db.seats);
