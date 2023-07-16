23fe8ecfc018aa52aa85f85e41f45d1333ce1c44
Eliza Hynek <eliza.hynek@gmail.com>
2023-07-16 19:21:35 +0200

    add mongoose

--- server.js
+++ server.js
@@ -1,21 +1,17 @@
 const express = require('express');
 const cors = require('cors');
-const uuid = require('uuid');
+//const uuid = require('uuid');
 const router = express.Router();
 const path = require('path');
-
+const mongoose = require('mongoose');
 const socket = require('socket.io');
-
-const app = express();
-const server = app.listen(process.env.PORT || 8000, () => {
-  console.log('Server is running on port: 8000');
-});
-const io = socket(server);
 
 
 const testimonialsRoutes = require('./routes/testimonials.routes');
 const concertsRoutes = require('./routes/concerts.routes');
 const seatsRoutes = require('./routes/seats.routes');
+
+const app = express();
 
 app.use((req, res, next) => {
   req.io = io;
@@ -39,6 +35,19 @@ app.use((req, res) => {
   res.status(400).send('Not found..');
 });
 
+const server = app.listen(process.env.PORT || 8000, () => {
+  console.log('Server is running on port: 8000');
+});
+const io = socket(server);
+
 io.on('connection', (socket) => {
   console.log('New socket ' + socket.id);
 });
+
+mongoose.connect('mongodb://localhost:27017/NewWaveDB', { useNewUrlParser: true });
+const db = mongoose.connection;
+
+db.once('open', () => {
+  console.log('Connected to the database');
+});
+db.on('error', err => console.log('Error ' + err));
